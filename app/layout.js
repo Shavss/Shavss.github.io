73cd6f20b9fb34682f58e358fe9a9f15a2b29438
Kacper <119957961+Shavss@users.noreply.github.com>
2026-03-21 13:14:30 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
 
@@ -11,6 +12,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={inter.variable}>
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-CKL34G5380"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-CKL34G5380');
+          `}
+        </Script>
+      </head>
       <body>{children}</body>
     </html>
   )
